9ce8b8322eba447fe5739bff3d2a4cc09ebcd31a
Alex Cortez <acim650@gmail.com>
2021-05-07 13:26:16 -0700

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 
-import React from 'react';
+import React, {useState} from 'react';
+
 
 function App() {
   return <div>
-    <h3>hello world!!!</h3>
-    <h4>goodbye</h4>
-    <Folder />
+    <Folder name='Desktop'>
+      <Folder name='music'>
+        <File name='all_star.mp4' />
+        <File name='express_file.mp4' />
+      </Folder>
+      <File name='dogs.jpeg' />
+      <File name='cats.png' />
+    </Folder>
+    <Folder name='Applications'/>
   </div>
 }
 
-const Folder = () => {
-  const name = 'my_desktop'
-  return <h4>{name}</h4>
+const Folder = (props) => {
+  console.log(useState(true));
+  const [ isOpen, setIsOpen ] = useState(true);
+  const { name, children} = props;
+  console.log(props);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+    console.log('clicked');
+  }
+
+  return <div>
+    <span onClick={handleClick}><i class='folder icon'></i></span>
+      {name}
+      <div style={{ marginLeft: '17px' }}>
+      {isOpen ? children : null}
+      </div>
+    </div>
+}
+
+const File = (props) => {
+  return <h5>{props.name}</h5>
 }
 
 export default App;
